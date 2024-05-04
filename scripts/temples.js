f40f0ce2a035fa36ea334a596950fb78b73b2f91
cragunbc <bccragun@yahoo.com>
2024-05-04 15:44:50 -0600

    Changes

--- scripts/temples.js
+++ scripts/temples.js
@@ -5,3 +5,12 @@ const today = new Date();
 
 year.innerHTML = today.getFullYear();
 modified.innerHTML =`Last Modified: ${document.lastModified}`;
+
+
+const hamButton = document.querySelector("#menu");
+const navigation = document.querySelector(".navigation");
+
+hamButton.addEventListener("click", () => {
+    navigation.classList.toggle("open");
+    hamButton.classList.toggle("open");
+});
